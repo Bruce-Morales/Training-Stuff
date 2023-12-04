a137e5a77398722407a49d6603385debb4a8ef41
Bruce-Morales <152554813+Bruce-Morales@users.noreply.github.com>
2023-12-04 14:01:49 -0800

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -2,8 +2,8 @@
             var image = document.getElementById("interactive-image");
             var images = [
                 "images/Dog 1",
-                "https://media.discordapp.net/attachments/1065413758475833344/1180269698684829726/IMG_8737.jpg?ex=657cced0&is=656a59d0&hm=4e0fe2b16b65c420610bf3258470c0b55b120c0e99494d63ac76d197d5f17c66&=&format=webp&width=647&height=660",
-                "https://cdn.discordapp.com/attachments/1065413758475833344/1180269798999994388/IMG_8786.jpg?ex=657ccee8&is=656a59e8&hm=1064917edcd8fc1bebcea8c81fc42365d386d1258ef448146a3e6aab4cc8edd2&"
+                "images/Dog 2",
+                "images/Dog 3"
             ];
             var currentIndex = 0;
 
