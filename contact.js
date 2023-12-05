9f21584d17a7c8aab77e15b3cc2931f3ea386e73
Bruce Morales <Bruce.s.morales@gmail.com>
2023-12-05 13:35:24 -0800

    added contact page

---
+++ contact.js
@@ -0,0 +1,9 @@
+const formkeepEmbed = document.querySelector('#formkeep-embed')
+
+formkeepEmbed.addEventListener('formkeep-embed:submitting', _event => {
+  console.log('Submitting form...')
+})
+
+formkeepEmbed.addEventListener('formkeep-embed:submitted', _event => {
+  console.log('Submitted form...')
+})
